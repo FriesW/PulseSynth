f6be256bc4ba498f32cc5244616a7c5dda6574d0
FriesW <william.james.fries@gmail.com>
2019-02-05 22:56:10 -0600

    Add Graph, fix bug
    Pd patch now has a basic signal graph
    Pd patch had a 'max' instead of 'min'

--- Relay.py
+++ Relay.py
@@ -6,7 +6,12 @@ p = input('Comport: ')
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 def bang():
     sock.sendto(b' ', ('127.0.0.1', 3344))
-
+def update(v):
+    v /= 0b1111111111
+    v *= 255
+    v = int(v)
+    v = min(v, 255)
+    sock.sendto(bytes([v]), ('127.0.0.1', 3345))
     
 high = False
 
@@ -14,6 +19,7 @@ with serial.Serial(p, 9600, timeout=5) as ser:
     ser.readline()
     while True:
         v = int(ser.readline().decode().strip())
+        update(v)
         if( v > 750 and not high):
             bang()
             high = True
